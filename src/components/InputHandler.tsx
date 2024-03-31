589b726f890a068adbde5396a15019a8b6e21fe7
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2024-03-30 21:59:28 -0400

    Terminal Box Created with Test Commands

--- src/components/InputHandler.tsx
+++ src/components/InputHandler.tsx
@@ -1,28 +1,38 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { VscChevronRight } from "react-icons/vsc";
+import { VscTriangleUp } from "react-icons/vsc";
 
 export default function InputHandler() {
-    const [command, setCommand] = useState<string>('');
+    const [command, setCommand] = useState<string>("");
     const [prevCommands, setPrevCommands] = useState<string[]>([]);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        console.log("Subbmited: ", command);
         setPrevCommands([...prevCommands, command]);
-        setCommand('');
-    }
+        setCommand("");
+    };
 
     return (
-        <form className="terminal-inputfield" onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={command}
-                onChange={e => setCommand(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-            <ul>
-                {prevCommands.map((prevCommand, index) => (
-                    <li key={index}>{prevCommand}</li>
-                ))}
-            </ul>
-        </form>
+        <div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <VscTriangleUp size={25} color="white" style={{ paddingLeft: 10 }} />
+                <h1 className="text-input">~/Programming/farhanj2.github.io</h1>
+            </div>
+            <div className="terminal-inputfield">
+                <VscChevronRight size={25} />
+                <input
+                    className="terminal-input"
+                    type="text"
+                    value={command}
+                    onChange={(e) => setCommand(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handleSubmit(e);
+                        }
+                    }}
+                />
+            </div>
+        </div>
     );
 }
