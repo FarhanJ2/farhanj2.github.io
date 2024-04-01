79286545916d1492a8b22fcad0135f247f3d9af7
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2024-04-01 11:26:57 -0400

    Fix Scaling Issues

--- src/components/InputHandler.tsx
+++ src/components/InputHandler.tsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
-import { VscChevronRight } from "react-icons/vsc";
-import { VscTriangleUp } from "react-icons/vsc";
+import React, { useState } from 'react';
+import { VscTriangleUp, VscChevronRight } from 'react-icons/vsc';
 
-export default function InputHandler() {
-    const [command, setCommand] = useState<string>("");
+const InputHandler = () => {
+    const [command, setCommand] = useState<string>('');
     const [prevCommands, setPrevCommands] = useState<string[]>([]);
+    const [currentCommand, setCurrentCommand] = useState<string>('');
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log("Subbmited: ", command);
+    const handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+        console.log('Command submitted:', command);
+        setCurrentCommand(command);
         setPrevCommands([...prevCommands, command]);
-        setCommand("");
+        setCommand('');
     };
 
     return (
         <div>
             <div style={{ display: "flex", alignItems: "center" }}>
-                <VscTriangleUp size={25} color="white" style={{ paddingLeft: 10 }} />
-                <h1 className="text-input">~/Programming/farhanj2.github.io</h1>
+                <VscTriangleUp size={25} color="white" />
+                <h1 className="text-input">~/Programming/farhanj2.github.io/{currentCommand}</h1>
             </div>
             <div className="terminal-inputfield">
                 <VscChevronRight size={25} />
                 <input
+                    placeholder="Type a command..."
                     className="terminal-input"
                     type="text"
                     value={command}
@@ -30,9 +31,17 @@ export default function InputHandler() {
                         if (e.key === "Enter") {
                             handleSubmit(e);
                         }
+                        if (e.key === "ArrowUp") {
+                            prevCommands.length > 0 ? setCommand(prevCommands[prevCommands.length - 1]) : null;
+                        }
+                        if (e.key === "ArrowDown") {
+                            setCommand('');
+                        }
                     }}
                 />
             </div>
         </div>
     );
-}
+};
+
+export default InputHandler;
